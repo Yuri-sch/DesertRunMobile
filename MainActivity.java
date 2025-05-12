9a62e78b15561ce35cbde40942e6d20c9d029e9f
Yuri-sch <110062311+Yuri-sch@users.noreply.github.com>
2025-05-12 01:02:28 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.desertrun;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.unity3d.player.UnityPlayerGameActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Abre a activity exportada da Unity
+        Intent intent = new Intent(this, UnityPlayerGameActivity.class);
+        startActivity(intent);
+    }
+}
